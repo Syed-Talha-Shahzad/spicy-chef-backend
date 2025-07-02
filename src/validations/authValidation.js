d03de206b5aaf455530be23893c10c67fa11615c
Syed-Talha-Shahzad <talhashahzad999@gmail.com>
2025-07-03 02:46:42 +0500

    first name last name validation added

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,4 +1,4 @@
-import { check,body } from "express-validator";
+import { check, body } from "express-validator";
 
 export const signInValidation = [
   check("email")
@@ -10,31 +10,36 @@ export const signInValidation = [
 ];
 
 export const signUpValidation = [
-    body('firstName')
-      .optional()
-      .isString().withMessage('First name must be a string'),
-  
-    body('lastName')
-      .optional()
-      .isString().withMessage('Last name must be a string'),
-  
-    body('email')
-      .notEmpty().withMessage('Email is required')
-      .isEmail().withMessage('Email is invalid'),
-  
-    body('password')
-      .notEmpty().withMessage('Password is required')
-      .isLength({ min: 8 }).withMessage('Password must be at least 8 characters'),
-  
-    body('address')
-      .optional()
-      .isString().withMessage('Address must be a string'),
-  
-    body('city')
-      .optional()
-      .isString().withMessage('City must be a string'),
-  
-    body('phoneNo')
-      .optional()
-      .isMobilePhone().withMessage('Phone number must be valid'),
-  ];
+  body("firstName")
+    .notEmpty()
+    .withMessage("First Name is required")
+    .isString()
+    .withMessage("First name must be a string"),
+
+  body("lastName")
+    .notEmpty()
+    .withMessage("Last Name is required")
+    .isString()
+    .withMessage("Last name must be a string"),
+
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Email is invalid"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters"),
+
+  body("address").optional().isString().withMessage("Address must be a string"),
+
+  body("city").optional().isString().withMessage("City must be a string"),
+
+  body("phoneNo")
+    .optional()
+    .isMobilePhone()
+    .withMessage("Phone number must be valid"),
+];
