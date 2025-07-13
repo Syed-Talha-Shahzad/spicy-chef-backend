44ec4b15505c7c85ca0c9a394ce0341b3319466a
Syed-Talha-Shahzad <talhashahzad999@gmail.com>
2025-07-13 12:28:50 +0500

    DELIEVERTIME ADDED

---
+++ prisma/migrations/20250713072549_add_deliverytimme_to_settings_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "setting" ADD COLUMN     "deliveryTime" DECIMAL(10,2) NOT NULL DEFAULT 0;
--- src/services/generalService.js
+++ src/services/generalService.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 class generalService {
   static async deliveryFee(req) {
     try {
-      const { branch_id, deliveryFee , serviceFee} = req.body;
+      const { branch_id, deliveryFee , serviceFee, deliveryTime} = req.body;
       const branch = await prisma.branch.findUnique({
         where: { id: branch_id },
       });
@@ -22,7 +22,7 @@ class generalService {
       if (setting) {
         const updatedSetting = await prisma.setting.update({
           where: { id: setting.id },
-          data: { deliveryFee, serviceFee },
+          data: { deliveryFee, serviceFee, deliveryTime },
         });
         return {
           status: true,
@@ -36,6 +36,7 @@ class generalService {
           data: {
             deliveryFee: deliveryFee,
             serviceFee: serviceFee,
+            deliveryTime: deliveryTime,
             branch_id: branch_id,
           },
         });
